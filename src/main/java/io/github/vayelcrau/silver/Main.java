024de6aff56c5a0e959d2c215edabaca1804d73a
Rishi Nath <rishi.nath@gmail.com>
2020-03-09 20:28:31 -0700

    initial commit

---
+++ src/main/java/io/github/vayelcrau/silver/Main.java
@@ -0,0 +1,12 @@
+package io.github.vayelcrau.silver;
+
+/**
+ * A simple web server that can serve HTML files that contain Javascript and other forms of non-code executing code.
+ * Added complexity would be to try and implement streaming video, create a server-side language, or serve up other stream types.
+ */
+
+public class Main {
+  public static void main(String... args) {
+
+  }
+}
